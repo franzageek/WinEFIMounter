56ee94f5147f6b9212ed9453a8bd8eeb34f3aa51
GiovsTechs <gianni.distazio009.6@gmail.com>
2023-07-22 16:47:33 +0200

    Update LinuxEFIMounter1.0.0.sh

--- LinuxEFIMounter1.0.0.sh
+++ LinuxEFIMounter1.0.0.sh
@@ -1,1 +1,38 @@
-!#/bin/bash
+#!/bin/bash
+
+# Check if it is opened as root
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must to be opened as a root"
+  exit 1
+fi
+
+
+echo       "#####################################################"
+echo      "#                   LinuxEFIMounter                   #"
+echo       "#####################################################"
+echo  Welcome to Linux. This program is meant to be used to access your
+echo  Hackintosh EFI from Linux. 
+echo
+echo  LinuxEFIMounter could be used to mount any EFI partition, but the main purpose of it
+echo  is to let you access your Hackintosh EFI partition from Linux since there is not
+echo  any tool to do that, apart mounting it by terminal.
+echo 
+read -n 1 -s -r -p "Press any key to continue"
+clear
+echo
+echo    "#####################################################"
+echo    "#                  LinuxEFIMounter                   #"
+echo    "#####################################################"
+echo
+echo  The EFI partition is currently NOT MOUNTED."
+echo
+echo  "1. Mount EFI partition to /mnt"
+echo
+echo  "E. Exit"
+
+
+read -p "Choose an option: " choice
+
+
+case $choice in
+
