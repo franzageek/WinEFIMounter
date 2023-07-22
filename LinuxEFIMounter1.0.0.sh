8f851db53089b7fbd42d14a522b1b213211197b0
GiovsTechs <gianni.distazio009.6@gmail.com>
2023-07-22 18:19:32 +0200

    Update LinuxEFIMounter1.0.0.sh

--- LinuxEFIMounter1.0.0.sh
+++ LinuxEFIMounter1.0.0.sh
@@ -2,35 +2,111 @@
 
 # Check if it is opened as root
 if [[ $EUID -ne 0 ]]; then
-  echo "This script must be opened as a root"
+  echo "This script must be run as a root."
   exit 1
 fi
 
-
-echo       "#####################################################"
-echo      "#                   LinuxEFIMounter                   #"
-echo       "#####################################################"
-echo  Welcome to LinuxEFIMounter. This program is meant to be used to access your
-echo  Hackintosh EFI from Linux. 
-echo
-echo  LinuxEFIMounter could be used to mount any EFI partition, but the main purpose of it
-echo  is to let you access your Hackintosh EFI partition from Linux since there is not
-echo  any tool to do that, apart from mounting it by boring commands".
-echo 
-read -n 1 -s -r -p "Press any key to continue"
+echo " "
+echo "   #####################################################"
+echo "  #                  LinuxEFIMounter                  #"
+echo " #####################################################"
+echo " "
+echo " Welcome to LinuxEFIMounter. This program is meant to be used to access your"
+echo " Hackintosh EFI from Linux. "
+echo " "
+echo " LinuxEFIMounter could be used to mount any EFI partition, but the main purpose of it"
+echo " is to let you access your Hackintosh EFI partition from Linux."
+echo " "
+echo " "
+read -s -r -p "   - Press any key to continue..."
 clear
-echo
-echo    "#####################################################"
-echo    "#                  LinuxEFIMounter                   #"
-echo    "#####################################################"
-echo
-echo  The EFI partition is currently NOT MOUNTED."
-echo
-echo  "1. Mount EFI partition to /mnt"
-echo
-echo  "E. Exit"
+echo " "
+echo "   #####################################################"
+echo "  #                  LinuxEFIMounter                  #"
+echo " #####################################################"
+echo " "
+echo " The EFI partition is currently NOT MOUNTED."
+echo " "
+echo " 1. Mount EFI partition to /mnt"
+echo " "
+echo " E. Exit"
+echo " " 
+echo " "
+
+
+while true; do
+  read -p "Please select a fuction: " function
+
+  case $function in
+    1)
+     
+      fdisk -l
+
+      echo "What is the path of the disk you want to mount?"
+      read -p "Disk path: " disk_path
+
+
+      temp_file=$(mktemp)
+
+   
+      fdisk -l | grep -w -c "$disk_path" >> $temp_file
+
+
+      if [[ $(cat $temp_file) -eq 1 ]]; then
+
+       
+        if [ ! -d /mnt ]; then
+          echo "Creating /mnt folder..."
+          mkdir /mnt
+        else
+          echo "/mnt folder already exists."
+        fi
+
+      
+        mount "$disk_path" /mnt
+      else
+        echo "Disk not found"
+      fi
+
+      rm $temp_file
+      break;;
+
+    E)
+      echo "Exiting..."
+      exit 0
+
+  esac
+done
 
 
 
+if [ -d /mnt ]; then
+  echo "The EFI partition is successfully mounted to /mnt."
+  echo "Please choose one of the following features:"
+  echo " "
+  echo "1. Navigate to the disk folder in the terminal"
+  echo "2. Navigate to the disk folder in the file manager"
+  echo "3. Unmount the disk"
+  echo " "
+  read -p "Please select a feature: " feature
+  case $feature in
+    1)
+      echo "Opening /mnt folder in the terminal..."
+      cd /mnt
+      ;;
 
+    2)
+      echo "this function is disabled"
+      
+      ;;
 
+    3)
+      echo "Unmounting disk..."
+      umount /mnt
+      echo "Disk unmounted."
+      ;;
+
+  esac
+else
+  echo "The EFI partition is not mounted."
+fi
