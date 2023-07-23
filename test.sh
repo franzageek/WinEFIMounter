9016b109084fa96f9b7bdd1bc17173fc246c3921
FranzaGeek <88248950+franzageek@users.noreply.github.com>
2023-07-23 11:33:42 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,27 @@
 
-function1 ()
-function2()
+function3 ()
 {
-   read -p “Press A to proceed or B to go back” choice
-   case $choice in
-      
-
-function3()
-{
-function 2
+echo "You chose to proceed."
+# some other code
 }
+
+function2 ()
+{
+    read -p “Press A to proceed or B to go back...” choice
+    case $choice in
+        A) 
+          function1
+          ;;
+        B)
+          function3
+          ;;
+    esac
+}
+
+function1 ()
+{
+    read -s -p "Press any key to begin..."
+    function2
+}
+
+function1
